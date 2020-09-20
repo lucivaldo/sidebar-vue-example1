6f36f9484c3e8f7ced03e6bfe3948a7d201af7a0
Lucivaldo Castro <lucivaldocb@gmail.com>
2020-09-19 21:06:41 -0300

    Adicionado Font Awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@fortawesome/fontawesome-free/js/all'
+
 Vue.config.productionTip = false
 
 new Vue({
